4ff96f20bbab129767db3410a054e710aa45e9c3
Pedro Jesus <pedrojesus.cefet@gmail.com>
2018-02-18 01:03:37 -0300

    Adicionar arquivos de projeto.

---
+++ FormCard/Controllers/MessagesController.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using FormCard.Dialogs;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using System.Linq;
+using System;
+
+namespace FormCard
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            var connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+            switch (activity.Type)
+            {
+                case ActivityTypes.Message:
+                    await Conversation.SendAsync(activity, () => new RootDialog());
+                    break;
+                case ActivityTypes.ConversationUpdate:
+                    if(activity.MembersAdded.Any(c=>c.Id == activity.Recipient.Id))
+                    {
+                        var resposta = activity.CreateReply();
+                        resposta.Text = "Digite algo para iniciar";
+                        await connector.Conversations.ReplyToActivityAsync(resposta);                        
+                    }
+                    break;                
+            }
+
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+
+       
+    }
+}
---
+++ FormCard/Dialogs/RootDialog.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace FormCard.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            var activity = await result as Activity;           
+
+            var resposta = activity.CreateReply();
+            //resposta.Attachments.Clear();
+            var card =  CriaFormulario("Titulo1");
+            resposta.Attachments.Add(card);
+
+            
+
+            //Aqui vou analisar o retorno
+            var retorno =  VerificaRetorno(activity.Text);
+
+            if (retorno != null)
+            {
+                resposta.Attachments.Clear();
+                resposta.Attachments.Add(retorno);
+
+            }
+            await context.PostAsync(resposta);
+        }
+
+        Attachment VerificaRetorno(string text)
+        {
+            if(text.Equals("opcao1", StringComparison.InvariantCultureIgnoreCase))
+            {
+                return CriaFormulario("retorno");
+            }
+            if(text.Equals("opcao2", StringComparison.InvariantCultureIgnoreCase))
+            {
+                return CriaFormulario("retorno");
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private Attachment CriaFormulario(string text)
+        {
+            var heroCard = new HeroCard
+            {
+                Buttons = new List<CardAction>
+            {
+                new CardAction
+                {
+                  Title = text,
+                  Type = ActionTypes.PostBack,
+                  Value = Opcao.opcao1.ToString()
+
+                },
+                new CardAction
+                {
+                    Title = "Titulo2",
+                    Type = ActionTypes.PostBack,
+                    Value = Opcao.opcao2.ToString()
+                }
+            }
+            };
+
+            return heroCard.ToAttachment();
+        }
+
+        enum Opcao
+        {
+            opcao1 = 1,
+            opcao2
+        }
+    }
+}
