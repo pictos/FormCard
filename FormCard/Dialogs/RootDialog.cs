187fce9141595d7024a97d12db7647c2987c60e7
Pedro Jesus <pedrojesus.cefet@gmail.com>
2018-02-18 01:04:04 -0300

    adsf

--- FormCard/Dialogs/RootDialog.cs
+++ FormCard/Dialogs/RootDialog.cs
@@ -12,7 +12,7 @@ namespace FormCard.Dialogs
         public Task StartAsync(IDialogContext context)
         {
             context.Wait(MessageReceivedAsync);
-
+            //
             return Task.CompletedTask;
         }
 
